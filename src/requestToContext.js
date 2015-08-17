f80e0d0c9998b39b8e2840040550c6245dfdaca1
dzannotti <d.zannotti@me.com>
2015-08-18 00:08:23 +0100

    matched reqToContext of express

--- src/requestToContext.js
+++ src/requestToContext.js
@@ -1,44 +1,25 @@
 "use strict";
 
-var getContext = function (req) {
-    // todo: not sure if in the below line should copy this object, or force Joi validation conversion
-    // perhaps it"s irrelevant, and this is more performant, verify.
-    var context = req.query;
-
-    // as far as i know, params cannot contain nested objects so this should be safe
-    for (var key in context) {
-        if ({}.hasOwnProperty.call(context, key)) {
-            context[key] = decodeURIComponent(context[key]);
-            if (key === "path") {
-                context[key] = JSON.parse(context[key]);
-            }
-        }
-    }
-    return context;
-};
-
-var postContext = function(req) {
-    var context = req.payload;
-    if(context.jsonGraph !== undefined) {
-        context.jsonGraph = JSON.parse(context.jsonGraph);
-    }
-    if(context.callPath !== undefined) {
-        context.callPath = JSON.parse(context.callPath);
-    }
-
-    return  context;
+var parseArgs = {
+    "jsonGraph": true,
+    "callPath": true,
+    "arguments": true,
+    "pathSuffixes": true,
+    "paths": true
 };
 
 module.exports = function requestToContext(req) {
+    var queryMap = req.method === "post" ? req.payload : req.query;
     var context = {};
-    if (req.method === "get") {
-        context = getContext(req);
-    }
-    else if (req.method === "post") {
-        context = postContext(req);
-    }
-    if (typeof context.paths === "string") {
-        context.paths = JSON.parse(context.paths);
+    if (queryMap) {
+        Object.keys(queryMap).forEach(function(key) {
+            var arg = queryMap[key];
+            if (parseArgs[key] && arg != null) {
+                context[key] = JSON.parse(arg);
+            } else {
+                context[key] = arg;
+            }
+        });
     }
     return context;
 };
